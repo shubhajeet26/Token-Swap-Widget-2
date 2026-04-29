f5214fdb2f23676574aeeb5dd3baedbb999dee25
shubhajeet26 <shubhajeetsaha26@gmail.com>
2026-04-29 22:36:16 +0530

    Revert static export for Vercel

--- client/next.config.ts
+++ client/next.config.ts
@@ -1,8 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  output: "export",
-  eslint: { ignoreDuringBuilds: true },
+  /* config options here */
 };
 
 export default nextConfig;
